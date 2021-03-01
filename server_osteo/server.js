ed194e7553e5b3505b49d1aab21faf693e22d2b5
MaxJGruber <maxjgruber@gmail.com>
2021-03-01 14:42:20 +0100

    removed routes from server

--- server_osteo/server.js
+++ server_osteo/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+// const serveStatic = require('serve-static')
+// const path = require('path')
 const cors = require("cors");
 
 const app = express()
@@ -17,13 +17,13 @@ const corsOptions = { origin: process.env.VUE_APP_FRONTEND_URL, credentials: tru
 
 app.use(cors(corsOptions));
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// app.use('/', serveStatic(path.join(__dirname, 'public')))
 app.use("/", cloudinaryRouter)
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
+// app.get(/.*/, function (req, res) {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// })
+console.log(__dirname)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
