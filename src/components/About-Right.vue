4cc5bca18f208be8bcdae14e4068ccbca196f476
MaxJGruber <maxjgruber@gmail.com>
2021-03-15 13:59:23 +0100

    added patient sections after receiving content from client: waiting approval from client

--- src/components/About-Right.vue
+++ src/components/About-Right.vue
@@ -2,28 +2,71 @@
   <div :id="segment.id" class="relative bg-white overflow-hidden about mb-10">
     <div class="max-w-7xl mx-auto">
       <main
-        class="mt-1 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-10"
+        class="mt-1 mx-auto max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:pr-5 xl:mt-10"
       >
-        <div class="sm:text-center lg:text-right">
+        <div class="grid">
+          <div class="about-pic-frame">
+            <img
+              class="h-auto w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full about-pic"
+              :src="segment.img"
+              alt=""
+            />
+          </div>
+          <div class="sm:text-center lg:text-right">
+            <h1
+              class="text-xl tracking-tight font-bold text-logo-gray sm:text-5xl md:text-xl"
+            >
+              <span class="block xl:inline">{{ segment.title }}</span>
+            </h1>
+            <p
+              class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-md sm:max-w-xl sm:mr-0 sm:ml-auto md:mt-5 md:text-lg"
+            >
+              {{ segment.intro }}
+            </p>
+          </div>
+        </div>
+        <div
+          v-show="segment.subtext1"
+          class="sm:text-center lg:text-left xl:mt-10"
+        >
           <h1
-            class="text-2xl tracking-tight font-bold text-logo-gray sm:text-5xl md:text-2xl"
+            class="text-lg tracking-tight font-bold text-logo-gray sm:text-5xl md:text-lg"
           >
-            <span class="block xl:inline">{{ segment.title }}</span>
+            <span class="block xl:inline">{{ segment.subtext1Title }}</span>
           </h1>
           <p
-            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mr-0 lg:pl-15"
+            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg md:mt-5 md:text-md lg:mr-4"
           >
-            {{ segment.intro }}
+            {{ segment.subtext1 }}
+          </p>
+        </div>
+        <div v-show="segment.subtext2" class="sm:text-center lg:text-left mt-8">
+          <h1
+            v-show="segment.subtext2Title"
+            class="text-lg tracking-tight font-bold text-logo-gray sm:text-5xl md:text-lg"
+          >
+            <span class="block xl:inline">{{ segment.subtext2Title }}</span>
+          </h1>
+          <p
+            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg md:mt-5 md:text-md lg:mr-4"
+          >
+            {{ segment.subtext2 }}
+          </p>
+        </div>
+        <div v-show="segment.subtext3" class="sm:text-center lg:text-left mt-8">
+          <h1
+            v-show="segment.subtext3Title"
+            class="text-lg tracking-tight font-bold text-logo-gray sm:text-5xl md:text-lg"
+          >
+            <span class="block xl:inline">{{ segment.subtext3Title }}</span>
+          </h1>
+          <p
+            class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg md:mt-5 md:text-md lg:mr-4"
+          >
+            {{ segment.subtext3 }}
           </p>
         </div>
       </main>
-    </div>
-    <div class="lg:absolute lg:inset-y-0 lg:w-1/2 about-pic-frame">
-      <img
-        class="h-auto w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full about-pic"
-        :src="segment.img"
-        alt=""
-      />
     </div>
   </div>
 </template>
@@ -38,16 +81,24 @@ export default {
 
 <style>
 .about {
-  height: 100%;
+  height: auto;
+}
+
+.grid {
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  /* margin-right: 10px;
+  margin-left: 10px; */
 }
 
 .about-pic-frame {
-  margin: 2%;
-  padding: 1%;
   width: fit-content;
+  display: flex;
 }
 .about-pic {
+  justify-content: center;
+  align-items: center;
   width: auto;
+  height: 325px;
   border-radius: 0.5em;
   border: 2px solid #3abfd6;
 }
--- src/views/Patients.vue
+++ src/views/Patients.vue
@@ -43,7 +43,7 @@ export default {
       },
       nourissons: {
         id: "nourissons",
-        title: "Les nourissons",
+        title: "La prise en charge ostéopathique des nourissons",
         intro: `L'ostéopathie est une pratique manuelle traitant les pertes de
             mobilité des structures corporelles tel que les articulations
             musculo-squelettiques, les viscères, les tissus... Les techniques de
@@ -55,31 +55,31 @@ export default {
       },
       enfants: {
         id: "enfants",
-        title: "Les enfants & adolescents",
-        intro: `L'ostéopathie est une pratique manuelle traitant les pertes de
-            mobilité des structures corporelles tel que les articulations
-            musculo-squelettiques, les viscères, les tissus... Les techniques de
-            manipulation permettent de corriger les dysfonctions dans les
-            respects des barrières anatomiques et physiologiques du corps.
-            L'objectif est de réguler l'homéostasie du corps et de permettre
-            l'autorégulation de celui-ci.`,
-        img: require("../assets/photo-gallery/shooting11.jpg"),
+        title: "La prise en charge ostéopathique des enfants & adolescents",
+        intro: `L'ostéopathie chez les enfants et adolescents est adaptée et recommandée pour différents motifs semblables à l'adulte. Cependant, des troubles de la statique sont plus fréquents à ces âges comme risque de lordoses et dysfonctions au niveau des courbures rachidiennes. Elles sont souvent dues à une mauvaise adaptation du corps par rapport à la croissance ou bien encore des troubles posturaux avec les sacs à dos lourd et mauvaise assise pendant les cours.`,
+        subtext1: `Votre ostéopathe Charles Dumeige emploiera des techniques ostéopathiques adaptées en fonction de l'âge (3-14 ans pour les enfants et 14-18 ans pour les adolescents). Il vous procurera des conseils au niveau hygiéno-diététique (pratiquer un sport régulier par exemple) ou encore vous expliquera les gestes et postures afin d'éviter ses troubles de la statique.`,
+        subtext2: `
+Il est recommandé de consulter une fois par an maximum, en fonction des besoins de l'enfant (bien rester attentif à ses plaintes et anticiper l'apparition de douleurs associées). Un parent devra être obligatoirement présent lors de la consultation et des conseils lui seront donnés également pour sensibiliser à surveiller l'évolution de la croissance de l'enfant. En effet, il devra surveiller l'évolution des courbures rachidiennes afin de détecter des possibles apparition de scoliose, pour qu'une prise en charge multidisciplinaire adaptée soit mise en place avant un stade trop avancé (séances de kinésithérapeute fortement recommandées à ce moment là en complément). L'ostéopathie participe à l'efficacité et à la stabilité du traitement.`,
+        img: require("../assets/photo-gallery/shooting5.jpg"),
       },
       femmesEnceintes: {
         id: "enceinte",
-        title: "Les femmes enceintes",
-        intro: `L'ostéopathie est une pratique manuelle traitant les pertes de
-            mobilité des structures corporelles tel que les articulations
-            musculo-squelettiques, les viscères, les tissus... Les techniques de
-            manipulation permettent de corriger les dysfonctions dans les
-            respects des barrières anatomiques et physiologiques du corps.
-            L'objectif est de réguler l'homéostasie du corps et de permettre
-            l'autorégulation de celui-ci.`,
-        img: require("../assets/photo-gallery/shooting5.jpg"),
+        title: "La prise en charge ostéopathique de la femme enceinte",
+        intro: `Durant la grossesse, le corps s'adapte en fonction du développement de l'enfant in utéro. Cela aura un impact sur les forces réparties dans le corps au niveau des charnières, des vertèbres pivot ainsi que sur les courbures (lordoses, cyphoses). Le bassin aussi se modèle et d'autres changements mécaniques et physiologiques se manifestent. L'ostéopathie est recommandée pour rééquilibrer le corps face à ces changements, en utilisant des techniques adaptées en fonction des trimestres de grossesse.`,
+        subtext1Title: `L'ostéopathie durant la grossesse et prévenir l'apparition de douleurs.`,
+        subtext1: `Parfois le corps n'arrive pas à s'adapter face à ces changements et les douleurs apparaissent. Elles peuvent être sous-jacentes à des adaptations avant la grossesse qui se décompensent à un moment donné de cette dernière. En général, il s'agit de douleurs au niveau lombaire, de sciatalgies et de douleurs au niveau du bassin (sacrum-coccyx). Il peut y avoir des également des dysfonctionnements sur la sphère viscérale (troubles du transit, ballonnements, douleurs abdominales) et sur la sphère crânienne (céphalées, migraines).
+
+Dans ces cas, il est recommandé de consulter durant la grossesse, chaque trimestre pour faire des suivis et s'orienter un axe de prévention pré et post accouchement.`,
+        subtext2Title: `L'ostéopathie après la grossesse.`,
+        subtext2: `On ne pense pas forcément à la phase de post partum pour la femme et son bien-être. Il est pourtant primordial de rester sur une bonne impression pour de nouvelles expériences à l'avenir et de soulager dans le nouveau rôle de maman. 
+
+Durant la phase de grossesse, le corps de la femme enceinte change constamment et même après l'accouchement il subit encore des modifications sur les mêmes sphères évoquées précédemment et il peut y avoir des douleurs persistantes. Il est recommandé de consulter un mois après la naissance ou après la rééducation périnéale (sans se substituer d'un suivi gynéco-obstétrical).`,
+        subtext3: `Votre ostéopathe Charles Dumeige prend en charge les premières consultations de femmes enceintes ainsi que les suivis de grossesse.`,
+        img: require("../assets/photo-gallery/shooting11.jpg"),
       },
       adultes: {
         id: "adultes",
-        title: "Les adultes & séniors",
+        title: "La prise en charge ostéopathique des adultes & séniors",
         intro: `L'ostéopathie est une pratique manuelle traitant les pertes de
             mobilité des structures corporelles tel que les articulations
             musculo-squelettiques, les viscères, les tissus... Les techniques de
@@ -91,7 +91,7 @@ export default {
       },
       sportifs: {
         id: "sportifs",
-        title: "Les sportifs",
+        title: "La prise en charge ostéopathique des sportifs",
         intro: `L'ostéopathie est une pratique manuelle traitant les pertes de
             mobilité des structures corporelles tel que les articulations
             musculo-squelettiques, les viscères, les tissus... Les techniques de
